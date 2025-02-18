c96dd7096ba7da0650b314b2b4f503bca49fd185
dev24anuj <anujbhardwaj1020@gmail.com>
2025-02-18 15:31:47 +0530

    code to compare 3 number

--- calci.py
+++ calci.py
@@ -3,3 +3,20 @@ num1= input('enter first number:' )
 num2= input('enter second number:' )
 sum = float(num1) + float(num2)
 print('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
+
+num1=input("enter the first number ")
+num2=input("enter the first number ")
+num3=input("enter the first number ")
+
+# def max_num(num1, num2, num3):
+if num1 >=num2  and num1 >=num3:
+        print(f"first number {num1} is largest")
+        # return num1
+elif num2 >=num1 and num2 >=num3:
+        print(f"second number {num2} is largest")
+        # return num2
+else:
+        print(f"third number {num3} is largest")
+
+# print(max_num)
