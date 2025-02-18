95909f98bcfd17a2261515180671e73676d3dbe7
dev24anuj <anujbhardwaj1020@gmail.com>
2025-02-18 16:12:44 +0530

    intrageted calculator

--- calci.py
+++ calci.py
@@ -20,3 +20,19 @@ else:
         print(f"third number {num3} is largest")
 
 # print(max_num)
+
+# calculator using python 
+
+num1 =float(input("enter the first number: "))
+op = input("enter the operator: ")
+num2 =float(input("enter the second number: "))
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
+else :
+    print("invalid operator")
